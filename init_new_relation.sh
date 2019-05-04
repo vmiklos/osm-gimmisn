b38cb456560c1797291d71cd8e040258fb85b07f
Miklos Vajna <vmiklos@collabora.com>
2019-05-04 09:09:59 +0200

    init_new_relation: work with the templates which are now in git

--- init_new_relation.sh
+++ init_new_relation.sh
@@ -10,13 +10,13 @@ value="$2"
 
 cd workdir
 
-sed "s/2714372/$value/g" street-housenumbers-sasad.txt > street-housenumbers-$name.txt
+sed "s/@RELATION@/$value/g" ../street-housenumbers-template.txt > street-housenumbers-$name.txt
 ../overpass_query.py street-housenumbers-$name.txt > street-housenumbers-$name.csv
 
 # Don't talk to overpass again without an interrupt.
 sleep 10s
 
-sed "s/2714372/$value/g" streets-sasad.txt > streets-$name.txt
+sed "s/@RELATION@/$value/g" ../streets-template.txt > streets-$name.txt
 ../overpass_query.py streets-$name.txt > streets-$name.csv
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
