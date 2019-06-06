36f902c4d9df2ea798c80a8e69a1222ff1f4fff6
Miklos Vajna <vmiklos@collabora.com>
2019-06-06 09:06:18 +0200

    Turn this class with no methods into a plain function

--- suspicious_streets.py
+++ suspicious_streets.py
@@ -10,48 +10,49 @@ suspicious as lots of house numbers are probably missing."""
 
 import os
 import sys
+import configparser
 # pylint: disable=unused-import
 from typing import Dict, List
-import configparser
 import helpers
 
 
-class Finder:
+def get_suspicious_streets(datadir, workdir, relation_name):
     """Compares reference house numbers with OSM ones and shows the diff."""
-    def __init__(self, datadir, workdir, relation_name):
-        self.normalizers = {}  # type: Dict[str, helpers.Ranges]
-        # OSM name -> ref name map
-        self.ref_streets = {}  # type: Dict[str, str]
+    normalizers = {}  # type: Dict[str, helpers.Ranges]
+    # OSM name -> ref name map
+    ref_streets = {}  # type: Dict[str, str]
 
-        self.normalizers, self.ref_streets = helpers.load_normalizers(datadir, relation_name)
-        street_names = helpers.get_streets(workdir, relation_name)
+    normalizers, ref_streets = helpers.load_normalizers(datadir, relation_name)
+    street_names = helpers.get_streets(workdir, relation_name)
 
-        results = []
-        both_results = []
+    results = []
+    both_results = []
 
-        for street_name in street_names:
+    for street_name in street_names:
 
-            ref_street = street_name
-            # See if we need to map the OSM name to ref name.
-            if street_name in self.ref_streets.keys():
-                ref_street = self.ref_streets[street_name]
+        ref_street = street_name
+        # See if we need to map the OSM name to ref name.
+        if street_name in ref_streets.keys():
+            ref_street = ref_streets[street_name]
 
-            reference_house_numbers = helpers.get_house_numbers_from_lst(workdir, relation_name, street_name,
-                                                                         ref_street, self.normalizers)
-            osm_house_numbers = helpers.get_house_numbers_from_csv(workdir, relation_name, street_name,
-                                                                   self.normalizers)
-            only_in_reference = helpers.get_only_in_first(reference_house_numbers, osm_house_numbers)
-            in_both = helpers.get_in_both(reference_house_numbers, osm_house_numbers)
-            if only_in_reference:
-                results.append((street_name, only_in_reference))
-            if in_both:
-                both_results.append((street_name, in_both))
+        reference_house_numbers = helpers.get_house_numbers_from_lst(workdir, relation_name, street_name,
+                                                                     ref_street, normalizers)
+        osm_house_numbers = helpers.get_house_numbers_from_csv(workdir, relation_name, street_name,
+                                                               normalizers)
+        only_in_reference = helpers.get_only_in_first(reference_house_numbers, osm_house_numbers)
+        in_both = helpers.get_in_both(reference_house_numbers, osm_house_numbers)
+        if only_in_reference:
+            results.append((street_name, only_in_reference))
+        if in_both:
+            both_results.append((street_name, in_both))
 
-        # Sort by length.
-        results.sort(key=lambda result: len(result[1]), reverse=True)
+    # Sort by length.
+    results.sort(key=lambda result: len(result[1]), reverse=True)
 
-        self.suspicious_streets = results
-        self.done_streets = both_results
+    suspicious_streets = results
+    done_streets = both_results
+
+    return suspicious_streets, done_streets
 
 
 def main():
@@ -65,9 +66,9 @@ def main():
     if len(sys.argv) > 1:
         relation_name = sys.argv[1]
 
-    finder = Finder(datadir, workdir, relation_name)
+    suspicious_streets, _ = get_suspicious_streets(datadir, workdir, relation_name)
 
-    for result in finder.suspicious_streets:
+    for result in suspicious_streets:
         if result[1]:
             # House number, # of only_in_reference items.
             print("%s\t%s" % (result[0], len(result[1])))
