e1d1dfa772d88ac120726cc0e55478a8639d4f25
Miklos Vajna <vmiklos@collabora.com>
2019-06-07 20:32:49 +0200

    get_reference_housenumbers: drop VERBOSE mode, we're fast enough now

--- get_reference_housenumbers.py
+++ get_reference_housenumbers.py
@@ -16,7 +16,6 @@ from typing import Dict
 from typing import List
 import helpers
 
-VERBOSE = False
 MEMORY_CACHE = {}  # type: Dict[str, Dict[str, Dict[str, List[str]]]]
 
 
@@ -53,8 +52,6 @@ def build_memory_cache(local):
 
 def house_numbers_of_street(datadir, reference, relation_name, street):
     """Gets house numbers for a street locally."""
-    if VERBOSE:
-        print("searching '" + street + "'")
     refmegye, reftelepules, street_name, street_type = helpers.get_street_details(datadir, street, relation_name)
     street = street_name + " " + street_type
     if street in reference[refmegye][reftelepules].keys():
@@ -69,8 +66,6 @@ def get_reference_housenumbers(config, relation_name):
     from OSM."""
     reference = config.get('wsgi', 'reference_local').strip()
     if not MEMORY_CACHE:
-        if VERBOSE:
-            print("building in-memory cache")
         build_memory_cache(reference)
 
     datadir = os.path.join(os.path.dirname(__file__), "data")
@@ -90,7 +85,6 @@ def get_reference_housenumbers(config, relation_name):
 
 def main():
     """Commandline interface to this module."""
-    global VERBOSE
 
     config = configparser.ConfigParser()
     config_path = os.path.join(os.path.dirname(__file__), "wsgi.ini")
@@ -99,7 +93,6 @@ def main():
     if len(sys.argv) > 1:
         relation_name = sys.argv[1]
 
-    VERBOSE = True
     get_reference_housenumbers(config, relation_name)
 
 
