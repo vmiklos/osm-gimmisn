652c254f51e99119f6fdef72d9bc4cc17afd1e55
vasony <50217163+vasony@users.noreply.github.com>
2019-10-08 10:15:17 +0200

    validator: win force utf8

--- validator.py
+++ validator.py
@@ -157,6 +157,10 @@ def validate_relations(errors: List[str], relations: Dict[str, Any]) -> None:
 
 def main() -> None:
     """Commandline interface to this module."""
+    if sys.platform.startswith("win"):
+        import _locale
+        # pylint: disable=protected-access
+        _locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 
     yaml_path = sys.argv[1]
     _, basename = os.path.split(yaml_path)
