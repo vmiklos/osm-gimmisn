b0bfd0d0f0136d24bc84085522973e39ee2d8622
Miklos Vajna <vmiklos@vmiklos.hu>
2019-12-28 21:09:33 +0100

    Test webframe.local_to_ui_tz()
    Tested-by: Travis

--- tests/test_webframe.py
+++ tests/test_webframe.py
@@ -10,8 +10,12 @@ from typing import List
 from typing import TYPE_CHECKING
 from typing import Tuple
 from typing import cast
+import configparser
+import datetime
+import os
 import unittest
 import unittest.mock
+import time
 
 import webframe
 
@@ -66,6 +70,28 @@ class TestHandleException(unittest.TestCase):
             self.assertIn("ValueError", output)
             return
         self.fail()
+
+
+class TestLocalToUiTz(unittest.TestCase):
+    """Tests local_to_ui_tz()."""
+    def test_happy(self) -> None:
+        """Tests the happy path."""
+        def get_abspath(path: str) -> str:
+            if os.path.isabs(path):
+                return path
+            return os.path.join(os.path.dirname(__file__), path)
+
+        def get_config() -> configparser.ConfigParser:
+            config = configparser.ConfigParser()
+            config.read_dict({"wsgi": {"timezone": "Europe/Budapest"}})
+            return config
+
+        with unittest.mock.patch('util.get_abspath', get_abspath):
+            with unittest.mock.patch('webframe.get_config', get_config):
+                local_dt = datetime.datetime.fromtimestamp(0)
+                ui_dt = webframe.local_to_ui_tz(local_dt)
+                if time.strftime('%Z%z') == "CET+0100":
+                    self.assertEqual(ui_dt.timestamp(), 0)
 
 
 if __name__ == '__main__':
