889efd2c82f9a8faf179bc3349b213212a7379ab
Miklos Vajna <vmiklos@vmiklos.hu>
2020-06-17 21:36:32 +0200

    test_stats: use test_config.TestCase
    Tested-by: Travis

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -13,14 +13,9 @@ import os
 import unittest
 import unittest.mock
 
+import config
 import stats
-
-
-def get_abspath(path: str) -> str:
-    """Mock get_abspath() that uses the test directory."""
-    if os.path.isabs(path):
-        return path
-    return os.path.join(os.path.dirname(__file__), path)
+import test_config
 
 
 def mock_strftime(_fmt: str) -> str:
@@ -41,206 +36,190 @@ class MockDate(datetime.date):
         return cls(2020, 5, 10)
 
 
-class TestHandleProgress(unittest.TestCase):
+class TestHandleProgress(test_config.TestCase):
     """Tests handle_progress()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('time.strftime', mock_strftime):
-                stats.handle_progress(src_root, j)
-            progress = j["progress"]
-            self.assertEqual(progress["date"], "2020-05-10")
-            self.assertEqual(progress["percentage"], 7.81)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('time.strftime', mock_strftime):
+            stats.handle_progress(src_root, j)
+        progress = j["progress"]
+        self.assertEqual(progress["date"], "2020-05-10")
+        self.assertEqual(progress["percentage"], 7.81)
 
     def test_old_time(self) -> None:
         """Tests the case when the .count file doesn't exist for a date."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('time.strftime', mock_strftime_old):
-                stats.handle_progress(src_root, j)
-            progress = j["progress"]
-            self.assertEqual(progress["date"], "1970-01-01")
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('time.strftime', mock_strftime_old):
+            stats.handle_progress(src_root, j)
+        progress = j["progress"]
+        self.assertEqual(progress["date"], "1970-01-01")
 
 
-class TestHandleTopusers(unittest.TestCase):
+class TestHandleTopusers(test_config.TestCase):
     """Tests handle_topusers()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('time.strftime', mock_strftime):
-                stats.handle_topusers(src_root, j)
-            topusers = j["topusers"]
-            self.assertEqual(len(topusers), 20)
-            self.assertEqual(topusers[0], ["user1", "68885"])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('time.strftime', mock_strftime):
+            stats.handle_topusers(src_root, j)
+        topusers = j["topusers"]
+        self.assertEqual(len(topusers), 20)
+        self.assertEqual(topusers[0], ["user1", "68885"])
 
     def test_old_time(self) -> None:
         """Tests the case when the .count file doesn't exist for a date."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('time.strftime', mock_strftime_old):
-                stats.handle_topusers(src_root, j)
-            topusers = j["topusers"]
-            self.assertFalse(topusers)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('time.strftime', mock_strftime_old):
+            stats.handle_topusers(src_root, j)
+        topusers = j["topusers"]
+        self.assertFalse(topusers)
 
 
-class TestHandleDailyNew(unittest.TestCase):
+class TestHandleDailyNew(test_config.TestCase):
     """Tests handle_daily_new()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                # From now on, today is 2020-05-10, so this will read 2020-04-26, 2020-04-27, etc
-                # (till a file is missing.)
-                stats.handle_daily_new(src_root, j)
-            daily = j["daily"]
-            self.assertEqual(len(daily), 1)
-            self.assertEqual(daily[0], ["2020-04-26", 364])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            # From now on, today is 2020-05-10, so this will read 2020-04-26, 2020-04-27, etc
+            # (till a file is missing.)
+            stats.handle_daily_new(src_root, j)
+        daily = j["daily"]
+        self.assertEqual(len(daily), 1)
+        self.assertEqual(daily[0], ["2020-04-26", 364])
 
     def test_empty_day_range(self) -> None:
         """Tests the case when the day range is empty."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            stats.handle_daily_new(src_root, j, day_range=-1)
-            daily = j["daily"]
-            self.assertFalse(daily)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        stats.handle_daily_new(src_root, j, day_range=-1)
+        daily = j["daily"]
+        self.assertFalse(daily)
 
 
-class TestHandleMonthlyNew(unittest.TestCase):
+class TestHandleMonthlyNew(test_config.TestCase):
     """Tests handle_monthly_new()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                stats.handle_monthly_new(src_root, j)
-            monthly = j["monthly"]
-            self.assertEqual(len(monthly), 2)
-            # 2019-05 start -> end
-            self.assertEqual(monthly[0], ["2019-05", 3799])
-            # diff from last month end -> today
-            self.assertEqual(monthly[1], ["2020-05", 51334])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            stats.handle_monthly_new(src_root, j)
+        monthly = j["monthly"]
+        self.assertEqual(len(monthly), 2)
+        # 2019-05 start -> end
+        self.assertEqual(monthly[0], ["2019-05", 3799])
+        # diff from last month end -> today
+        self.assertEqual(monthly[1], ["2020-05", 51334])
 
     def test_empty_month_range(self) -> None:
         """Tests the case when the month range is empty."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            stats.handle_monthly_new(src_root, j, month_range=-1)
-            monthly = j["monthly"]
-            self.assertTrue(monthly)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        stats.handle_monthly_new(src_root, j, month_range=-1)
+        monthly = j["monthly"]
+        self.assertTrue(monthly)
 
     def test_incomplete_last_month(self) -> None:
         """Tests the case when we have no data for the last, incomplete month."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                # This would be the data for the current state of the last, incomplete month.
-                hide_path = get_abspath("workdir/stats/2020-05-10.count")
-                real_exists = os.path.exists
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            # This would be the data for the current state of the last, incomplete month.
+            hide_path = config.get_abspath("workdir/stats/2020-05-10.count")
+            real_exists = os.path.exists
 
-                def mock_exists(path: str) -> bool:
-                    if path == hide_path:
-                        return False
-                    return real_exists(path)
-                with unittest.mock.patch('os.path.exists', mock_exists):
-                    stats.handle_monthly_new(src_root, j)
-            monthly = j["monthly"]
-            # 1st element: 2019-05 start -> end
-            # No 2nd element, would be diff from last month end -> today
-            self.assertEqual(len(monthly), 1)
-            self.assertEqual(monthly[0], ["2019-05", 3799])
+            def mock_exists(path: str) -> bool:
+                if path == hide_path:
+                    return False
+                return real_exists(path)
+            with unittest.mock.patch('os.path.exists', mock_exists):
+                stats.handle_monthly_new(src_root, j)
+        monthly = j["monthly"]
+        # 1st element: 2019-05 start -> end
+        # No 2nd element, would be diff from last month end -> today
+        self.assertEqual(len(monthly), 1)
+        self.assertEqual(monthly[0], ["2019-05", 3799])
 
 
-class TestHandleDailyTotal(unittest.TestCase):
+class TestHandleDailyTotal(test_config.TestCase):
     """Tests handle_daily_total()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                stats.handle_daily_total(src_root, j)
-            dailytotal = j["dailytotal"]
-            self.assertEqual(len(dailytotal), 1)
-            self.assertEqual(dailytotal[0], ["2020-04-27", 251614])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            stats.handle_daily_total(src_root, j)
+        dailytotal = j["dailytotal"]
+        self.assertEqual(len(dailytotal), 1)
+        self.assertEqual(dailytotal[0], ["2020-04-27", 251614])
 
     def test_empty_day_range(self) -> None:
         """Tests the case when the day range is empty."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            stats.handle_daily_total(src_root, j, day_range=-1)
-            dailytotal = j["dailytotal"]
-            self.assertFalse(dailytotal)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        stats.handle_daily_total(src_root, j, day_range=-1)
+        dailytotal = j["dailytotal"]
+        self.assertFalse(dailytotal)
 
 
-class TestHandleUserTotal(unittest.TestCase):
+class TestHandleUserTotal(test_config.TestCase):
     """Tests handle_user_total()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                stats.handle_user_total(src_root, j)
-            usertotal = j["usertotal"]
-            self.assertEqual(len(usertotal), 1)
-            self.assertEqual(usertotal[0], ["2020-04-27", 43])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            stats.handle_user_total(src_root, j)
+        usertotal = j["usertotal"]
+        self.assertEqual(len(usertotal), 1)
+        self.assertEqual(usertotal[0], ["2020-04-27", 43])
 
     def test_empty_day_range(self) -> None:
         """Tests the case when the day range is empty."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            stats.handle_user_total(src_root, j, day_range=-1)
-            usertotal = j["usertotal"]
-            self.assertFalse(usertotal)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        stats.handle_user_total(src_root, j, day_range=-1)
+        usertotal = j["usertotal"]
+        self.assertFalse(usertotal)
 
 
-class TestHandleMonthlyTotal(unittest.TestCase):
+class TestHandleMonthlyTotal(test_config.TestCase):
     """Tests handle_monthly_total()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                stats.handle_monthly_total(src_root, j)
-            monthlytotal = j["monthlytotal"]
-            self.assertEqual(len(monthlytotal), 1)
-            self.assertEqual(monthlytotal[0], ['2019-05', 203317])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            stats.handle_monthly_total(src_root, j)
+        monthlytotal = j["monthlytotal"]
+        self.assertEqual(len(monthlytotal), 1)
+        self.assertEqual(monthlytotal[0], ['2019-05', 203317])
 
     def test_empty_day_range(self) -> None:
         """Tests the case when the day range is empty."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            stats.handle_monthly_total(src_root, j, month_range=-1)
-            monthlytotal = j["monthlytotal"]
-            self.assertFalse(monthlytotal)
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        stats.handle_monthly_total(src_root, j, month_range=-1)
+        monthlytotal = j["monthlytotal"]
+        self.assertFalse(monthlytotal)
 
     def test_one_element_day_range(self) -> None:
         """Tests the case when the day range is of just one element."""
-        with unittest.mock.patch('config.get_abspath', get_abspath):
-            src_root = get_abspath("workdir/stats")
-            j: Dict[str, Any] = {}
-            with unittest.mock.patch('datetime.date', MockDate):
-                stats.handle_monthly_total(src_root, j, month_range=0)
-            monthlytotal = j["monthlytotal"]
-            self.assertEqual(len(monthlytotal), 2)
-            self.assertEqual(monthlytotal[0], ["2020-04", 253027])
-            self.assertEqual(monthlytotal[1], ["2020-05", 254651])
+        src_root = config.get_abspath("workdir/stats")
+        j: Dict[str, Any] = {}
+        with unittest.mock.patch('datetime.date', MockDate):
+            stats.handle_monthly_total(src_root, j, month_range=0)
+        monthlytotal = j["monthlytotal"]
+        self.assertEqual(len(monthlytotal), 2)
+        self.assertEqual(monthlytotal[0], ["2020-04", 253027])
+        self.assertEqual(monthlytotal[1], ["2020-05", 254651])
 
 
 if __name__ == '__main__':
