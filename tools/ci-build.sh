49ec0d0b80e397ab83fdf66f9fd543e0316bcafb
Miklos Vajna <vmiklos@vmiklos.hu>
2020-11-09 18:21:19 +0100

    CI: try to paralellize the build
    Change-Id: Iade462fa42edb3b8aaabbade112c2147c862bced

---
+++ tools/ci-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+#
+# Copyright 2020 Miklos Vajna. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+#
+# This script runs all the tests for CI purposes.
+#
+
+if [ -n "${GITHUB_WORKFLOW}" ]; then
+    sudo apt-get install gettext
+fi
+pip install -r requirements.txt
+npm install
+make -j$(getconf _NPROCESSORS_ONLN) check
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
