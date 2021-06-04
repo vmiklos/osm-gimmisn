ba6762b0375edf9bf8179baeb308fabc34f58faf
Miklos Vajna <vmiklos@vmiklos.hu>
2021-06-05 00:14:00 +0200

    Port to config.Config2.get_timezone()
    Which means Config.has_value() is now unused.
    
    Change-Id: Ia8a20aa6d749183e95eda6e68342e56388544d23

--- tests/test_webframe.py
+++ tests/test_webframe.py
@@ -120,12 +120,12 @@ class TestLocalToUiTz(test_config.TestCase):
     """Tests local_to_ui_tz()."""
     def test_happy(self) -> None:
         """Tests the happy path."""
-        config.Config.set_value("timezone", "Europe/Budapest")
+        conf = config.Config2("tests")
+        conf.set_value("timezone", "Europe/Budapest")
         local_dt = datetime.datetime.fromtimestamp(0)
-        ui_dt = webframe.local_to_ui_tz(local_dt)
-        if time.strftime('%Z%z') == "CET+0100":
+        ui_dt = webframe.local_to_ui_tz(conf, local_dt)
+        if time.strftime('%Z%z') == "CEST+0200":
             self.assertEqual(ui_dt.timestamp(), 0)
-        config.Config.set_value("timezone", "")
 
 
 class TestFillMissingHeaderItems(unittest.TestCase):
