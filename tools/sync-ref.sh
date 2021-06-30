439978bae335d26ace7d8cb6994826dc6c0bbc07
Miklos Vajna <vmiklos@vmiklos.hu>
2021-06-30 20:56:39 +0200

    Update reference to 20210629
    Change-Id: Idcb74dbfcbc22855747a5309651ec5a0665bbb23

---
+++ tools/sync-ref.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+#
+# Copyright 2021 Miklos Vajna. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+#
+# This script synchronizes reference data between a public instance and a local dev instance.
+#
+
+refs=$(grep -o '[a-z0-9_]\+.tsv' data/wsgi.ini.template)
+mkdir refdir.new
+cd refdir.new
+for i in $refs
+do
+    wget $1/$i
+done
+cd ..
+rm -rf refdir
+mv refdir.new refdir
+cat data/wsgi.ini.template > wsgi.ini
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
