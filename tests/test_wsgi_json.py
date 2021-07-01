aa3ca03fdac1a5740d5aa930306a9582995b3adf
Miklos Vajna <vmiklos@vmiklos.hu>
2021-07-01 20:24:12 +0200

    test_wsgi_json: avoid mocking
    Still 63 calls to unittest.mock.patch() remains.
    
    Change-Id: I73a5c7184569b290d768a27ca96a5dab0c913301

--- tests/test_wsgi_json.py
+++ tests/test_wsgi_json.py
@@ -7,18 +7,13 @@
 """The test_wsgi_json module covers the wsgi_json module."""
 
 from typing import Any
-from typing import BinaryIO
 from typing import Dict
 from typing import List
-from typing import Optional
 from typing import TYPE_CHECKING
 from typing import Tuple
 from typing import cast
-import io
 import json
 import unittest
-import unittest.mock
-import urllib.error
 
 import test_config
 
@@ -71,12 +66,15 @@ class TestJsonStreets(TestWsgiJson):
     def test_update_result_json_error(self) -> None:
         """Tests if the update-result json output on error is well-formed."""
         conf = test_config.make_test_config()
-
-        def mock_urlopen(_url: str, _data: Optional[bytes] = None) -> BinaryIO:
-            raise urllib.error.HTTPError(url="", code=0, msg="", hdrs={}, fp=io.BytesIO())
-        with unittest.mock.patch('urllib.request.urlopen', mock_urlopen):
-            root = self.get_json_for_path(conf, "/streets/gazdagret/update-result.json")
-            self.assertEqual(root["error"], "HTTP Error 0: ")
+        routes: List[test_config.URLRoute] = [
+            test_config.URLRoute(url="https://overpass-api.de/api/interpreter",
+                                 data_path="",
+                                 result_path="")
+        ]
+        network = test_config.TestNetwork(routes)
+        conf.set_network(network)
+        root = self.get_json_for_path(conf, "/streets/gazdagret/update-result.json")
+        self.assertIn("error", root)
 
 
 class TestJsonStreetHousenumbers(TestWsgiJson):
@@ -84,35 +82,28 @@ class TestJsonStreetHousenumbers(TestWsgiJson):
     def test_update_result_json(self) -> None:
         """Tests if the update-result output is well-formed."""
         conf = test_config.make_test_config()
-        result_from_overpass = "@id\taddr:street\taddr:housenumber\taddr:postcode\taddr:housename\t"
-        result_from_overpass += "addr:conscriptionnumber\taddr:flats\taddr:floor\taddr:door\taddr:unit\tname\t@type\n\n"
-        result_from_overpass += "1\tTörökugrató utca\t1\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tTörökugrató utca\t2\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tTűzkő utca\t9\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tTűzkő utca\t10\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tOSM Name 1\t1\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tOSM Name 1\t2\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tOnly In OSM utca\t1\t\t\t\t\t\t\t\t\tnode\n"
-        result_from_overpass += "1\tSecond Only In OSM utca\t1\t\t\t\t\t\t\t\t\tnode\n"
-
-        def mock_urlopen(_url: str, _data: Optional[bytes] = None) -> BinaryIO:
-            buf = io.BytesIO()
-            buf.write(result_from_overpass.encode('utf-8'))
-            buf.seek(0)
-            return buf
-        with unittest.mock.patch('urllib.request.urlopen', mock_urlopen):
-            root = self.get_json_for_path(conf, "/street-housenumbers/gazdagret/update-result.json")
-            self.assertEqual(root["error"], "")
+        routes: List[test_config.URLRoute] = [
+            test_config.URLRoute(url="https://overpass-api.de/api/interpreter",
+                                 data_path="",
+                                 result_path="tests/network/overpass-housenumbers-gazdagret.csv")
+        ]
+        network = test_config.TestNetwork(routes)
+        conf.set_network(network)
+        root = self.get_json_for_path(conf, "/street-housenumbers/gazdagret/update-result.json")
+        self.assertEqual(root["error"], "")
 
     def test_update_result_error_json(self) -> None:
         """Tests if the update-result output on error is well-formed."""
         conf = test_config.make_test_config()
-
-        def mock_urlopen(_url: str, _data: Optional[bytes] = None) -> BinaryIO:
-            raise urllib.error.HTTPError(url="", code=0, msg="", hdrs={}, fp=io.BytesIO())
-        with unittest.mock.patch('urllib.request.urlopen', mock_urlopen):
-            root = self.get_json_for_path(conf, "/street-housenumbers/gazdagret/update-result.json")
-            self.assertEqual(root["error"], "HTTP Error 0: ")
+        routes: List[test_config.URLRoute] = [
+            test_config.URLRoute(url="https://overpass-api.de/api/interpreter",
+                                 data_path="",
+                                 result_path="")
+        ]
+        network = test_config.TestNetwork(routes)
+        conf.set_network(network)
+        root = self.get_json_for_path(conf, "/street-housenumbers/gazdagret/update-result.json")
+        self.assertIn("error", root)
 
 
 class TestJsonMissingHousenumbers(TestWsgiJson):
