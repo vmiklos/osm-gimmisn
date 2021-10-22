04ebe008589ae7a2e0268112c8e8f15b55dbcf30
Miklos Vajna <vmiklos@vmiklos.hu>
2021-10-22 21:39:36 +0200

    Port up to tests.test_areas.TestRefcountyGetRefsettlement
    Change-Id: I60bc85a2ae63a31d7cb83e38863efc7bc39a0bbe

--- tests/test_areas.py
+++ tests/test_areas.py
@@ -11,52 +11,6 @@ import unittest
 import test_context
 
 import areas
-import rust
-
-
-class TestRelationConfigMissingStreets(unittest.TestCase):
-    """Tests RelationConfig.should_check_missing_streets()."""
-    def test_nosuchrelation(self) -> None:
-        """Tests a relation without a filter file."""
-        relation_name = "nosuchrelation"
-        relations = areas.make_relations(test_context.make_test_context())
-        relation = relations.get_relation(relation_name)
-        ret = relation.get_config().should_check_missing_streets()
-        self.assertEqual(ret, "yes")
-
-
-class TestRelationConfigLetterSuffixStyle(unittest.TestCase):
-    """Tests RelationConfig.get_letter_suffix_style()."""
-    def test_happy(self) -> None:
-        """Tests the happy path."""
-        relation_name = "empty"
-        relations = areas.make_relations(test_context.make_test_context())
-        relation = relations.get_relation(relation_name)
-        self.assertEqual(relation.get_config().get_letter_suffix_style(), rust.PyLetterSuffixStyle.upper())
-        config = relation.get_config()
-        config.set_letter_suffix_style(rust.PyLetterSuffixStyle.lower())
-        relation.set_config(config)
-        self.assertEqual(relation.get_config().get_letter_suffix_style(), rust.PyLetterSuffixStyle.lower())
-
-
-class TestRefmegyeGetName(unittest.TestCase):
-    """Tests refcounty_get_name()."""
-    def test_happy(self) -> None:
-        """Tests the happy path."""
-        ctx = test_context.make_test_context()
-        relations = areas.make_relations(ctx)
-        self.assertEqual(relations.refcounty_get_name("01"), "Budapest")
-        self.assertEqual(relations.refcounty_get_name("99"), "")
-
-
-class TestRefmegyeGetReftelepulesIds(unittest.TestCase):
-    """Tests refcounty_get_refsettlement_ids()."""
-    def test_happy(self) -> None:
-        """Tests the happy path."""
-        ctx = test_context.make_test_context()
-        relations = areas.make_relations(ctx)
-        self.assertEqual(relations.refcounty_get_refsettlement_ids("01"), ["011", "012"])
-        self.assertEqual(relations.refcounty_get_refsettlement_ids("99"), [])
 
 
 class TestReftelepulesGetName(unittest.TestCase):
