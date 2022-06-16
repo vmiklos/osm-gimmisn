ca1470aab145da87dbeecc03f55a62980ce3ae80
Miklos Vajna <vmiklos@vmiklos.hu>
2022-06-16 07:19:16 +0200

    validator: test_relation_housenumber_letters_bad: fold test file into code
    Change-Id: Iae1ee619305aed4537fc9cb518655de3039c7b6f

--- src/validator/tests.rs
+++ src/validator/tests.rs
@@ -11,8 +11,6 @@
 //! Tests for the validator module.
 
 use super::*;
-use std::cell::RefCell;
-use std::rc::Rc;
 
 /// Tests main(): valid relations.
 #[test]
@@ -158,12 +156,15 @@ fn assert_failure_msg(path: &str, expected: &str) {
 }
 
 /// Asserts that a given input (path, content) fails with a given error message.
-fn assert_failure_msg2(content: &Rc<RefCell<std::io::Cursor<Vec<u8>>>>, expected: &str) {
+fn assert_failure_msg2(content: &str, expected: &str) {
     let path = "data/relation-myrelation.yaml";
     let mut ctx = context::tests::make_test_context().unwrap();
     let argv: &[String] = &["".into(), ctx.get_abspath(path)];
     let mut buf: std::io::Cursor<Vec<u8>> = std::io::Cursor::new(Vec::new());
-    let files = context::tests::TestFileSystem::make_files(&ctx, &[(path, &content)]);
+
+    let file = context::tests::TestFileSystem::make_file();
+    file.borrow_mut().write_all(content.as_bytes()).unwrap();
+    let files = context::tests::TestFileSystem::make_files(&ctx, &[(path, &file)]);
     let file_system = context::tests::TestFileSystem::from_files(&files);
     ctx.set_file_system(&file_system);
 
@@ -360,45 +361,30 @@ Caused by:
 /// Tests the housenumber-letters key: bad type.
 #[test]
 fn test_relation_housenumber_letters_bad() {
-    let expected = r#"failed to validate tests/data/relation-gazdagret-housenumber-letters-bad.yaml
+    let content = "housenumber-letters: 42\n";
+    let expected = r#"failed to validate {0}
 
 Caused by:
     housenumber-letters: invalid type: integer `42`, expected a boolean at line 1 column 22
 "#;
-    assert_failure_msg(
-        "tests/data/relation-gazdagret-housenumber-letters-bad.yaml",
-        expected,
-    );
+    assert_failure_msg2(content, expected);
 }
 
 /// Tests the relation path: bad alias subkey.
 #[test]
 fn test_relation_alias_bad() {
-    let content = context::tests::TestFileSystem::make_file();
-    content
-        .borrow_mut()
-        .write_all(
-            br#"alias: [1]
-"#,
-        )
-        .unwrap();
+    let content = "alias: [1]\n";
     let expected = "expected value type for 'alias[0]' is str\nfailed to validate {0}\n";
-    assert_failure_msg2(&content, expected);
+    assert_failure_msg2(content, expected);
 }
 
 /// Tests the relation path: bad type for the alias subkey.
 #[test]
 fn test_relation_filters_alias_bad_type() {
-    let content = context::tests::TestFileSystem::make_file();
-    content
-        .borrow_mut()
-        .write_all(
-            br#"alias: "hello"
-"#,
-        )
-        .unwrap();
+    let content = r#"alias: "hello"
+"#;
     let expected = "failed to validate {0}\n\nCaused by:\n    alias: invalid type: string \"hello\", expected a sequence at line 1 column 8\n";
-    assert_failure_msg2(&content, expected);
+    assert_failure_msg2(content, expected);
 }
 
 /// Tests the relation path: bad filters -> show-refstreet value type.
