b115363defb2b83efae3920bef6822ef7a62caa6
Miklos Vajna <vmiklos@vmiklos.hu>
2022-06-28 08:10:33 +0200

    Rename 'cache_yamls' to 'osm-gimmisn cache_yamls'
    Mostly because each binary is linked statically, the debug size of them
    is around 82M, so combinding the various binaries into a (technically) a
    single one helps a lot.
    
    The other binaries are not yet touched.
    
    Change-Id: I49cef02289dd918bc0e32615652946346f7563f2

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2022 Miklos Vajna. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#![deny(warnings)]
+#![warn(clippy::all)]
+#![warn(missing_docs)]
+
+//! Provides the 'osm_gimmisn' cmdline tool.
+
+use std::collections::HashMap;
+use std::io::Write;
+
+type Handler = fn(&[String], &mut dyn Write, &osm_gimmisn::context::Context) -> i32;
+
+lazy_static::lazy_static! {
+    static ref HANDLERS: HashMap<String, Handler> = {
+        let mut ret: HashMap<String, Handler> = HashMap::new();
+        ret.insert("cache_yamls".into(), osm_gimmisn::cache_yamls::main);
+        ret
+    };
+}
+
+fn usage() {
+    println!("Valid commands:\n");
+    for (k, _v) in HANDLERS.iter() {
+        println!("{}", k);
+    }
+}
+
+fn main() {
+    let mut args: Vec<String> = std::env::args().collect();
+    let ctx = osm_gimmisn::context::Context::new("").unwrap();
+    let command = match args.get(1) {
+        Some(value) => value.to_string(),
+        None => {
+            println!("osm_gimmisn: missing command\n");
+            usage();
+            std::process::exit(1);
+        }
+    };
+    args.remove(1);
+    let handlers: Vec<&Handler> = HANDLERS
+        .iter()
+        .filter(|(k, _v)| *k == &command)
+        .map(|(_k, v)| v)
+        .collect();
+    let handler: &Handler = match handlers.get(0) {
+        Some(value) => value,
+        None => {
+            println!("osm_gimmisn: invalid command\n");
+            usage();
+            std::process::exit(1);
+        }
+    };
+
+    std::process::exit(handler(&args, &mut std::io::stdout(), &ctx))
+}
