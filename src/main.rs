daccda930ee73b44a74f85133859a4323b6e91ec
Miklos Vajna <vmiklos@vmiklos.hu>
2022-10-20 18:20:26 +0200

    Add a new sync-ref subcommand
    Handles the update of data/wsgi.ini.template, before tools/sync-ref.sh
    can be invoked.
    
    Change-Id: I31b1d4d8c07dc4d00066763ade43dc3e1d1e6d60

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,109 @@ fn cron_main(args: &[String], stream: &mut dyn Write, ctx: &osm_gimmisn::context
     osm_gimmisn::cron::main(args, stream, ctx)
 }
 
+/// This handles the update of data/wsgi.ini.template, tools/sync-ref.sh has to be still invoked
+/// after this.
+/// TODO merge tools/sync-ref.sh into this function.
+fn sync_ref_main(
+    args: &[String],
+    _stream: &mut dyn Write,
+    _ctx: &osm_gimmisn::context::Context,
+) -> i32 {
+    // TODO move this to a library and write tests.
+    // Download HTML.
+    use isahc::config::Configurable as _;
+    use isahc::ReadResponseExt as _;
+    use isahc::RequestExt as _;
+
+    let mut args_iter = args.iter();
+    let _self = args_iter.next();
+    let url = match args_iter.next() {
+        Some(s) => s,
+        None => {
+            println!("usage: osm-gimmisn sync-ref https://www.example.com/osm/data/");
+            return 1;
+        }
+    };
+    // let html = std::fs::read_to_string("osm-data.html").unwrap();
+    let mut buf = isahc::Request::get(url)
+        .redirect_policy(isahc::config::RedirectPolicy::Limit(1))
+        .body(())
+        .unwrap()
+        .send()
+        .unwrap();
+    let html: String = buf.text().unwrap();
+
+    // Parse the HTML.
+    let dom = html_parser::Dom::parse(&html).unwrap();
+    let mut dom_iter = dom.children.iter();
+    let mut root = dom_iter.next().unwrap();
+    if root.text().is_some() {
+        // Skip a first-line comment before the real root.
+        root = dom_iter.next().unwrap();
+    }
+    let root = root.into_iter();
+
+    // The format is type_date.tsv, figure out the latest date for each type.
+    let mut files: HashMap<String, u64> = HashMap::new();
+    for node in root {
+        if node.element().is_none() {
+            continue;
+        }
+
+        let element = node.element().unwrap();
+        if element.name != "a" {
+            continue;
+        }
+
+        let href_value: Option<String> = element.attributes.get("href").unwrap().clone();
+        let mut href = href_value.unwrap();
+        if !href.ends_with(".tsv") {
+            continue;
+        }
+
+        href = href.strip_suffix(".tsv").unwrap().into();
+        let tokens: Vec<&str> = href.split('_').collect();
+        let file: String = tokens[0..tokens.len() - 1].join("_");
+        let href_date: u64 = tokens[tokens.len() - 1].parse().unwrap();
+        files
+            .entry(file)
+            .and_modify(|date| *date = std::cmp::max(*date, href_date))
+            .or_insert(href_date);
+    }
+
+    // Generate config.
+    let mut config: Vec<String> = Vec::new();
+    config.push("[wsgi]".into());
+    config.push(format!(
+        "reference_housenumbers = refdir/hazszamok_{}.tsv refdir/hazszamok_kieg_{}.tsv",
+        files["hazszamok"], files["hazszamok_kieg"]
+    ));
+    config.push(format!(
+        "reference_street = refdir/utcak_{}.tsv",
+        files["utcak"]
+    ));
+    config.push(format!(
+        "reference_citycounts = refdir/varosok_count_{}.tsv",
+        files["varosok_count"]
+    ));
+    config.push(format!(
+        "reference_zipcounts = refdir/irsz_count_{}.tsv",
+        files["irsz_count"]
+    ));
+
+    // TODO get rid of these
+    config.push("tcp_port = 8000".into());
+    config.push("overpass_uri = https://z.overpass-api.de".into());
+    config.push("cron_update_inactive = False".into());
+    config.push(String::new());
+
+    // Write config.
+    let config_file = "data/wsgi.ini.template";
+    std::fs::write(config_file, config.join("\n")).unwrap();
+    println!("Updated {} successfully.", config_file);
+    0
+}
+
 lazy_static::lazy_static! {
     static ref HANDLERS: HashMap<String, Handler> = {
         let mut ret: HashMap<String, Handler> = HashMap::new();
@@ -81,6 +184,7 @@ lazy_static::lazy_static! {
         ret.insert("missing_housenumbers".into(), osm_gimmisn::missing_housenumbers::main);
         ret.insert("parse_access_log".into(), osm_gimmisn::parse_access_log::main);
         ret.insert("rouille".into(), rouille_main);
+        ret.insert("sync-ref".into(), sync_ref_main);
         ret.insert("validator".into(), osm_gimmisn::validator::main);
         ret
     };
@@ -97,6 +201,8 @@ fn main() {
     let parse_access_log = clap::Command::new("parse_access_log")
         .about("Parses the Apache access log of osm-gimmisn for 1 month");
     let rouille = clap::Command::new("rouille").about("Starts the web interface");
+    let sync_ref = clap::Command::new("sync-ref")
+        .about("Synchronizes the reference data from a public instance to a local dev instance");
     let validator = clap::Command::new("validator").about("Validates yaml files under data/");
     let subcommands = vec![
         cache_yamls,
@@ -104,6 +210,7 @@ fn main() {
         missing_housenumbers,
         parse_access_log,
         rouille,
+        sync_ref,
         validator,
     ];
     let app = clap::Command::new("osm-gimmisn").subcommand_required(true);
