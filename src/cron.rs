vmiklos/osm-gimmisn

commit 651b191f732f3030fc2fc05b144676cbbee1331f

Clean up tcp_port from wsgi.ini.template
It's enough to document the default.

Also update to latest clap to fix the build, because otherwise it would
fail with:

	error: failed to select a version for the requirement `textwrap = "^0.15.1"`

(wtf?)

Change-Id: Ia5850c17020ed2fac74d19355cee2c64b4080e7a

Cargo.toml
@@ -7,7 +7,7 @@ edition = "2021"
 accept-language = "2.0.0"
 anyhow = "1.0.65"
 chrono = "0.4.22"
-clap = "=3.2.22"
+clap = "=4.0.18"
 configparser = "3.0.2"
 csv = "1.1.6"
 gettext = "0.4.0"

src/cron.rs
@@ -528,7 +528,7 @@ fn update_stats(ctx: &context::Context, overpass: bool) -> anyhow::Result<()> {
 fn our_main_inner(
     ctx: &context::Context,
     relations: &mut areas::Relations,
-    mode: &str,
+    mode: &String,
     update: bool,
     overpass: bool,
 ) -> anyhow::Result<()> {
@@ -570,23 +570,22 @@ pub fn our_main(
 
     let refcounty = clap::Arg::new("refcounty")
         .long("refcounty")
-        .takes_value(true)
         .help("limit the list of relations to a given refcounty");
     let refsettlement = clap::Arg::new("refsettlement")
         .long("refsettlement")
-        .takes_value(true)
         .help("limit the list of relations to a given refsettlement");
     // Default: true.
     let no_update = clap::Arg::new("no-update")
         .long("no-update")
+        .action(clap::ArgAction::SetTrue)
         .help("don't update existing state of relations");
     let mode = clap::Arg::new("mode")
         .long("mode")
-        .takes_value(true)
         .default_value("relations")
         .help("only perform the given sub-task or all of them [all, stats or relations]");
     let no_overpass = clap::Arg::new("no-overpass") // default: true
         .long("no-overpass")
+        .action(clap::ArgAction::SetTrue)
         .help("when updating stats, don't perform any overpass update");
     let args = [refcounty, refsettlement, no_update, mode, no_overpass];
     let app = clap::Command::new("osm-gimmisn");
@@ -598,17 +597,17 @@ pub fn our_main(
     let first_day_of_month = now.date().day() == 1;
     relations.activate_all(ctx.get_ini().get_cron_update_inactive() || first_day_of_month);
     relations.activate_new();
-    let refcounty = args.value_of("refcounty");
+    let refcounty: Option<&String> = args.get_one("refcounty");
     relations.limit_to_refcounty(&refcounty)?;
     // Use map(), which handles optional values.
-    let refsettlement = args.value_of("refsettlement");
+    let refsettlement: Option<&String> = args.get_one("refsettlement");
     relations.limit_to_refsettlement(&refsettlement)?;
-    let update = !args.is_present("no-update");
-    let overpass = !args.is_present("no-overpass");
+    let update = !args.contains_id("no-update");
+    let overpass = !args.contains_id("no-overpass");
     our_main_inner(
         ctx,
         &mut relations,
-        args.value_of("mode").unwrap(),
+        args.get_one("mode").unwrap(),
         update,
         overpass,
     )?;

data/wsgi.ini.template
@@ -3,6 +3,5 @@ reference_housenumbers = refdir/hazszamok_20221001.tsv refdir/hazszamok_kieg_202
 reference_street = refdir/utcak_20221015.tsv
 reference_citycounts = refdir/varosok_count_20221001.tsv
 reference_zipcounts = refdir/irsz_count_20221001.tsv
-tcp_port = 8000
 overpass_uri = https://z.overpass-api.de
 cron_update_inactive = False

src/areas.rs
@@ -1337,7 +1337,7 @@ impl Relations {
     }
 
     /// If refcounty is not None, forget about all relations outside that refcounty.
-    pub fn limit_to_refcounty(&mut self, refcounty: &Option<&str>) -> anyhow::Result<()> {
+    pub fn limit_to_refcounty(&mut self, refcounty: &Option<&String>) -> anyhow::Result<()> {
         let refcounty: String = match refcounty {
             Some(value) => value.to_string(),
             None => {
@@ -1358,7 +1358,10 @@ impl Relations {
     }
 
     /// If refsettlement is not None, forget about all relations outside that refsettlement.
-    pub fn limit_to_refsettlement(&mut self, refsettlement: &Option<&str>) -> anyhow::Result<()> {
+    pub fn limit_to_refsettlement(
+        &mut self,
+        refsettlement: &Option<&String>,
+    ) -> anyhow::Result<()> {
         let refsettlement: String = match refsettlement {
             Some(value) => value.to_string(),
             None => {

src/areas/tests.rs
@@ -2494,7 +2494,9 @@ fn test_relations() {
             .contains(&"myrelation2".to_string()),
         true
     );
-    relations.limit_to_refcounty(&Some("01")).unwrap();
+    relations
+        .limit_to_refcounty(&Some(&"01".to_string()))
+        .unwrap();
     assert_eq!(
         relations
             .get_active_names()
@@ -2527,7 +2529,9 @@ fn test_relations() {
             .contains(&"myrelation3".to_string()),
         true
     );
-    relations.limit_to_refsettlement(&Some("99")).unwrap();
+    relations
+        .limit_to_refsettlement(&Some(&"99".to_string()))
+        .unwrap();
     assert_eq!(
         relations
             .get_active_names()

src/cron/tests.rs
@@ -1012,7 +1012,7 @@ fn test_our_main() {
     our_main_inner(
         &ctx,
         &mut relations,
-        /*mode=*/ "relations",
+        /*mode=*/ &"relations".to_string(),
         /*update=*/ true,
         /*overpass=*/ true,
     )
@@ -1113,7 +1113,7 @@ fn test_our_main_stats() {
     our_main_inner(
         &ctx,
         &mut relations,
-        /*mode=*/ "stats",
+        /*mode=*/ &"stats".to_string(),
         /*update=*/ false,
         /*overpass=*/ true,
     )

src/main.rs
@@ -164,7 +164,6 @@ fn sync_ref_main(
     ));
 
     // TODO get rid of these
-    config.push("tcp_port = 8000".into());
     config.push("overpass_uri = https://z.overpass-api.de".into());
     config.push("cron_update_inactive = False".into());
     config.push(String::new());

commit 0c591d671c156ba8d91a76e23baa155c3cfbb3c3

cron: fix default of the --no-update and --no-overpass switches
These are meant to be off by default, i.e. update and overpass are true
by default.

Regression from commit 651b191f732f3030fc2fc05b144676cbbee1331f (Clean
up tcp_port from wsgi.ini.template, 2022-10-24).

Change-Id: Ice94d133febf409d9cd467329816061f53c67c05

Cargo.toml
@@ -6,7 +6,7 @@ edition = "2021"
 [dependencies]
 accept-language = "2.0.0"
 anyhow = "1.0.66"
-chrono = "0.4.22"
+chrono = "=0.4.22"
 clap = "=4.0.18"
 configparser = "3.0.2"
 csv = "1.1.6"

src/cron.rs
@@ -601,8 +601,8 @@ pub fn our_main(
     // Use map(), which handles optional values.
     let refsettlement: Option<&String> = args.get_one("refsettlement");
     relations.limit_to_refsettlement(&refsettlement)?;
-    let update = !args.contains_id("no-update");
-    let overpass = !args.contains_id("no-overpass");
+    let update = !args.get_one::<bool>("no-update").unwrap();
+    let overpass = !args.get_one::<bool>("no-overpass").unwrap();
     our_main_inner(
         ctx,
         &mut relations,