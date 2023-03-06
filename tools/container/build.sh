e900d27889067976980e9077d9b8e894fe412b36
Miklos Vajna <vmiklos@vmiklos.hu>
2023-03-06 07:04:38 +0100

    tools, container: import initial support
    Docs still needs writing, but in short:
    
    - build.sh can build a container if you want to run osm-gimmisn on some
      hosting platform that only allows containers, not VMs
    
      - Containerfile is used by build.sh
    
    - run.sh can run the container, in a rootless mode
    
      - the container will invoke init.sh as its first / only process
    
    Change-Id: Ic0b467b88d950cc2dd4c9e1116349d6aebd1fff3

---
+++ tools/container/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Copyright 2023 Miklos Vajna
+#
+# SPDX-License-Identifier: MIT
+#
+
+podman build -f Containerfile -t osm-gimmisn
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
