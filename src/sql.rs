59831b62f05bbdf6e6cf542d38adc6e0f473b1f0
Miklos Vajna <vmiklos@vmiklos.hu>
2023-06-03 20:05:18 +0200

    sql: create initial tables based on pragma user_version
    Allows adding new columns to existing tables in a next commit.
    
    Change-Id: Ibe68d9fb7392d2a529598ce55fa0d0754d2c0e20

--- src/sql.rs
+++ src/sql.rs
@@ -10,53 +10,60 @@
 
 //! Database schema creation / migration.
 
+use anyhow::Context as _;
+
 pub fn init(conn: &rusqlite::Connection) -> anyhow::Result<()> {
-    conn.execute(
-        "create table if not exists ref_housenumbers (
+    let mut stmt = conn.prepare("pragma user_version")?;
+    let mut rows = stmt.query([])?;
+    let row = rows.next()?.context("no row")?;
+    let user_version: i64 = row.get(0).context("no col")?;
+    if user_version < 1 {
+        conn.execute(
+            "create table ref_housenumbers (
             county_code text not null,
             settlement_code text not null,
             street text not null,
             housenumber text not null,
             comment text not null
          )",
-        [],
-    )?;
-    conn.execute(
-        "create index if not exists idx_ref_housenumbers
+            [],
+        )?;
+        conn.execute(
+            "create index idx_ref_housenumbers
             on ref_housenumbers (county_code, settlement_code, street)",
-        [],
-    )?;
-    conn.execute(
-        "create table if not exists ref_streets (
+            [],
+        )?;
+        conn.execute(
+            "create table ref_streets (
             county_code text not null,
             settlement_code text not null,
             street text not null
          )",
-        [],
-    )?;
-    conn.execute(
-        "create index if not exists idx_ref_streets
+            [],
+        )?;
+        conn.execute(
+            "create index idx_ref_streets
             on ref_streets (county_code, settlement_code)",
-        [],
-    )?;
-    conn.execute(
-        "create table if not exists osm_housenumber_coverages (
+            [],
+        )?;
+        conn.execute(
+            "create table osm_housenumber_coverages (
             relation_name text primary key not null,
             coverage text not null,
             last_modified text not null
          )",
-        [],
-    )?;
-    conn.execute(
-        "create table if not exists osm_street_coverages (
+            [],
+        )?;
+        conn.execute(
+            "create table osm_street_coverages (
              relation_name text primary key not null,
              coverage text not null,
              last_modified text not null
          )",
-        [],
-    )?;
-    conn.execute(
-        "create table if not exists stats_invalid_addr_cities (
+            [],
+        )?;
+        conn.execute(
+            "create table stats_invalid_addr_cities (
             osm_id text not null,
             osm_type text not null,
             postcode text not null,
@@ -65,15 +72,19 @@ pub fn init(conn: &rusqlite::Connection) -> anyhow::Result<()> {
             housenumber text not null,
             user text not null
         )",
-        [],
-    )?;
-    conn.execute(
-        "create table if not exists mtimes (
+            [],
+        )?;
+        conn.execute(
+            "create table mtimes (
             page text primary key not null,
             last_modified text not null
         )",
-        [],
-    )?;
-    conn.execute("pragma user_version = 1", [])?;
+            [],
+        )?;
+        conn.execute("pragma user_version = 1", [])?;
+    }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests;
