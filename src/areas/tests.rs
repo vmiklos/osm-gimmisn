0c44a604a68e3fe19d14fdff519f2761b239397b
Miklos Vajna <vmiklos@vmiklos.hu>
2023-12-28 08:53:53 +0100

    areas: produce query for a relation's housenumbers, in json format
    Similar to commit 69960e4131d799ac6f99174341bfcec2c022d9a9 (areas:
    produce query for a relation's streets, in json format, 2023-11-17).
    
    Change-Id: Ia2f44997a98f0c318944879cd1bbbb5384ef3966

--- src/areas/tests.rs
+++ src/areas/tests.rs
@@ -731,6 +731,41 @@ fn test_relation_get_osm_housenumbers_query() {
     assert_eq!(ret, "housenr aaa 42 bbb 3600000042 ccc\n");
 }
 
+/// Tests Relation.get_osm_housenumbers_json_query().
+#[test]
+fn test_relation_get_osm_housenumbers_json_query() {
+    let mut ctx = context::tests::make_test_context().unwrap();
+    let yamls_cache = serde_json::json!({
+        "relations.yaml": {
+            "gazdagret": {
+                "osmrelation": 42,
+            },
+        },
+    });
+    let yamls_cache_value = context::tests::TestFileSystem::write_json_to_file(&yamls_cache);
+    let overpass_template = context::tests::TestFileSystem::make_file();
+    overpass_template
+        .borrow_mut()
+        .write_all(b"[out:csv(::id)] [timeout:425];\nhousenr aaa @RELATION@ bbb @AREA@ ccc\n")
+        .unwrap();
+    let files = context::tests::TestFileSystem::make_files(
+        &ctx,
+        &[
+            ("data/yamls.cache", &yamls_cache_value),
+            (
+                "data/street-housenumbers-template.overpassql",
+                &overpass_template,
+            ),
+        ],
+    );
+    let file_system = context::tests::TestFileSystem::from_files(&files);
+    ctx.set_file_system(&file_system);
+    let mut relations = Relations::new(&ctx).unwrap();
+    let relation = relations.get_relation("gazdagret").unwrap();
+    let ret = relation.get_osm_housenumbers_json_query().unwrap();
+    assert_eq!(ret, "[out:json];\nhousenr aaa 42 bbb 3600000042 ccc");
+}
+
 /// Tests RelationFiles.write_osm_streets().
 #[test]
 fn test_relation_files_write_osm_streets() {
