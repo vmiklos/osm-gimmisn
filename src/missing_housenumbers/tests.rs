460102e3fc00f9041f0f710c7e735d754b086fda
Miklos Vajna <vmiklos@vmiklos.hu>
2024-07-10 09:04:51 +0200

    areas, tests: move gh195 housenumbers ref to sql
    Towards tests not asserting internal details of get_ref_housenumbers(),
    3 more to go.
    
    Change-Id: I88432356e0caed502b6eb7a76b858431598b06cc

--- src/missing_housenumbers/tests.rs
+++ src/missing_housenumbers/tests.rs
@@ -21,11 +21,40 @@ fn test_main() {
     let argv = vec!["".to_string(), "gh195".to_string()];
     let mut buf: std::io::Cursor<Vec<u8>> = std::io::Cursor::new(Vec::new());
     let mut ctx = context::tests::make_test_context().unwrap();
+    let yamls_cache = serde_json::json!({
+        "relations.yaml": {
+            "gh195": {
+                "refcounty": "0",
+                "refsettlement": "0",
+                "osmrelation": 42,
+            },
+        },
+    });
+    let yamls_cache_value = context::tests::TestFileSystem::write_json_to_file(&yamls_cache);
+    let ref_file = context::tests::TestFileSystem::make_file();
+    let files = context::tests::TestFileSystem::make_files(
+        &ctx,
+        &[
+            ("data/yamls.cache", &yamls_cache_value),
+            ("workdir/street-housenumbers-reference-gh195.lst", &ref_file),
+        ],
+    );
+    let file_system = context::tests::TestFileSystem::from_files(&files);
+    ctx.set_file_system(&file_system);
     {
         let conn = ctx.get_database_connection().unwrap();
         conn.execute_batch(
-            "insert into osm_streets (relation, osm_id, name, highway, service, surface, leisure, osm_type) values ('gh195', '24746223', 'Kalotaszeg utca', 'residential', '', 'asphalt', '', '');
+            "insert into ref_housenumbers (county_code, settlement_code, street, housenumber, comment) values ('0', '0', 'Kalotaszeg utca', '25', '');
+             insert into ref_housenumbers (county_code, settlement_code, street, housenumber, comment) values ('0', '0', 'Kalotaszeg utca', '27-37', '');
+             insert into ref_housenumbers (county_code, settlement_code, street, housenumber, comment) values ('0', '0', 'Kalotaszeg utca', '31', 'CIVIL');
+             insert into osm_streets (relation, osm_id, name, highway, service, surface, leisure, osm_type) values ('gh195', '24746223', 'Kalotaszeg utca', 'residential', '', 'asphalt', '', '');
              insert into osm_streets (relation, osm_id, name, highway, service, surface, leisure, osm_type) values ('gh195', '695548547', 'Kalotaszeg utca', 'residential', '', '', '', '');").unwrap();
+    }
+    {
+        let mut relations = areas::Relations::new(&ctx).unwrap();
+        let relation_name = "gh195";
+        let relation = relations.get_relation(relation_name).unwrap();
+        relation.write_ref_housenumbers().unwrap();
     }
 
     let ret = main(&argv, &mut buf, &mut ctx);
