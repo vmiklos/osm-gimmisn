e8dfd5bff1bac76a4c802eaccfcac5660fbe434a
Miklos Vajna <vmiklos@vmiklos.hu>
2026-02-02 21:49:40 +0100

    sync-ref: update to HTML4
    If this changes again, a regex replace will be better.
    
    Change-Id: I06b8397379661954b88271e888f16342f642fded

--- src/sync_ref.rs
+++ src/sync_ref.rs
@@ -131,7 +131,7 @@ pub fn our_main(
     let mut html = ctx.get_network().urlopen(url, "")?;
     // Work around 'expected attribute key' failure.
     html = html.replace(
-        r#"<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">"#,
+        r#"<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">"#,
         r#"<!DOCTYPE html>"#,
     );
 
